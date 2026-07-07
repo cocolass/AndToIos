2de33d0c75976f4a0f7270be988eb397dc442e47
cocolass <34852965+cocolass@users.noreply.github.com>
2026-07-08 00:25:04 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/andtoios/bridge/SmsActivity.kt
@@ -0,0 +1,11 @@
+package com.andtoios.bridge
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SmsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish()
+    }
+}
