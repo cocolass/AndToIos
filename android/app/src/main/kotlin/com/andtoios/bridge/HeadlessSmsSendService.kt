e7c492026f051c230cbd450502478dd3161b746e
cocolass <34852965+cocolass@users.noreply.github.com>
2026-07-07 14:16:50 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/andtoios/bridge/HeadlessSmsSendService.kt
@@ -0,0 +1,9 @@
+package com.andtoios.bridge
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class HeadlessSmsSendService : Service() {
+    override fun onBind(intent: Intent?): IBinder? = null
+}
