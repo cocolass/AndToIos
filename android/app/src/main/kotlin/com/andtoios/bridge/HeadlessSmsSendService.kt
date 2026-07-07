60d061c7a9f47842573b3b68ff38b6bdba81ccd7
cocolass <34852965+cocolass@users.noreply.github.com>
2026-07-07 19:56:49 +0300

    Add logging for SMS service binding

--- android/app/src/main/kotlin/com/andtoios/bridge/HeadlessSmsSendService.kt
+++ android/app/src/main/kotlin/com/andtoios/bridge/HeadlessSmsSendService.kt
@@ -3,9 +3,11 @@ package com.andtoios.bridge
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 
 class HeadlessSmsSendService : Service() {
     override fun onBind(intent: Intent?): IBinder? {
+        Log.d("AndToIosSMS", "SMS Servis Bağlandı")
         return null
     }
 }
